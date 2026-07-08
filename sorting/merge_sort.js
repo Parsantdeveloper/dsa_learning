53967c53ecb90d201cb4cc9314e2ec18988ddfee
Prashant Bhusal <parshantbhusal1928@gmail.com>
2026-07-08 10:20:57 +0545

    learned merge_sort (not confident)

---
+++ sorting/merge_sort.js
@@ -0,0 +1,50 @@
+function merge(left, right) {
+
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+
+        if (left[i] <= right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    while (i < left.length) {
+        result.push(left[i]);
+        i++;
+    }
+
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+
+    return result;
+}
+
+function mergeSort(nums) {
+
+    if (nums.length <= 1) {
+        return nums;
+    }
+
+    const mid = Math.floor(nums.length / 2);
+
+    const left = nums.slice(0, mid);
+    const right = nums.slice(mid);
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    );
+}
+
+let nums = [5,1,1,2,0,0];
+
+console.log(mergeSort(nums));
